4d8d9fefd3a07bc5998c006f807334f13594091d
radvin226 <r68359337@gmail.com>
2026-08-07 11:35:35 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ import time
 
 BOT_TOKEN = "8956404018:AAHdIV2Jhv9FOK9Xs9qqwxsX-AKofRt-fb4" 
 
-HF_TOKEN = "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"
+HF_TOKEN = "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"
 
 OWNER_ID = 6420547446
 
